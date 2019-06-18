9233e27cf452733decb911d070d067dd87f2dfbd
younit <mac@MacdeMacBook-Pro.local>
2019-06-18 22:24:45 +0800

    add

---
+++ sun/src/api/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+axios.defaults.withCredentials = true
+
+let base = 'http://localhost:3000' //  local Address
+
+let config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+//  登录
+export const login = params => { return axios.post(`${ base }/users/login`, params, config).then(res => res.data) }
+
+export const users = params => { return axios.get(`${base}/users`, { params: params }) }
+
+export const goods = params => { return axios.get(`${base}/goods`, { params: params }) }
---
+++ sun/src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home">
+      <div id="regNum"></div>
+      {{ userslist }}
+  </div>
+</template>
+
+<script>
+import { users, goods } from '../api'
+export default {
+  name: 'home',
+  data () {
+    return {
+      options: {
+        title: {
+          text: 'Number of registered persons'
+        },
+      },
+      userslist: [],
+    }
+  },
+  mounted() {
+    let regNum = this.$echarts.init(document.getElementById('regNum'), 'dark')
+    this.setEcharts(regNum, this.options)
+    this.getusersList()
+
+    let para = {}
+    goods(para).then((res) => {
+      console.log(res)
+    })
+  },
+  methods: {
+    /**
+      @method 设置图像
+      @param {obj, obj} dom对象, 配置参数
+     */
+    setEcharts (obj, options) {
+      obj.setOption(options)
+    },
+    getusersList () { //  查询用户列表
+      let para = {}
+      users(para).then((res) => {
+        console.log(res)
+        this.userslist = res.data
+      })
+    }
+    
+  },
+}
+</script>
+<style lang="stylus" scoped>
+#regNum {
+  width 500px
+  height  300px
+}
+</style>
+
