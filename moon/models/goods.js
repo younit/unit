00fbaafc5830254455402d5af9095c32fa1ad63e
younit <younit@aliyun.com>
2019-06-20 18:09:23 +0800

    del

--- moon/models/goods.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const productSchema = new Schema({
-  'name': String,
-  'price': Number
-})
-module.exports = mongoose.model('goods', productSchema)
--- moon/models/users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const productSchema = new Schema({
-  'name': String,
-  'pwd': String,
-  'status': Number
-})
-module.exports = mongoose.model('users', productSchema)
